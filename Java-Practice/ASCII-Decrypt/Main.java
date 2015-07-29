28be8c0bfdb7904fcc51cc0ac922dfd42bf8572e
Andrew Grant <andrewgrant243@gmail.com>
2015-07-28 22:53:38 -0500

    ASCII Decrypt Code Solved

--- Java-Practice/ASCII-Decrypt/Main.java
+++ Java-Practice/ASCII-Decrypt/Main.java
@@ -16,21 +16,16 @@ public class Main {
 			}
 			String[] initialSplit = line.split(" ");
 			int lengthOfWord = Integer.parseInt(initialSplit[0]);
-			//System.out.println("length of word: " + lengthOfWord);
 			String knownLetter = initialSplit[2];
-			//System.out.println("known letter: " + knownLetter);
 			int[] codes = new int[initialSplit.length - 4];
 			for(int i = 0 ; i < codes.length; i++) {
 				codes[i] = Integer.parseInt(initialSplit[i+4]);	
 			}			
-			for(int i = 0; i < codes.length; i++) {
-				//System.out.println(codes[i]);
-			}
+			//printCodes(codes);
 			int shift  = findMessage(codes, lengthOfWord, knownLetter);
-			//System.out.println(a);
 			for(int i = 0; i < codes.length; i++) {
 				char tc = (char)(codes[i] - shift);
-				//System.out.print(tc);
+				System.out.print(tc);
 			}
 			//System.out.print("abc");
 		}
@@ -38,117 +33,37 @@ public class Main {
 
 	private static int findMessage(int[] arr, int len, String let) {
 
-	ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();	
+		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();	
 		int shifter = -1;
+		char letChar = let.charAt(0);
 		String word = "";
 		for(int i = 0; i < (arr.length - len); i++) {
-			for(int a = i + 5; a < (arr.length - len); a++) {
-				if(arr[i] == arr[a]) {
-					//System.out.println("here");
-					if(arr[i+1] == arr[a+1]) {
-						if(arr[i+2] == arr[a+2]) {
-							if(arr[i+3] == arr[a+3]) {
-								if(arr[i+4] == arr[a+4]) {
-									ArrayList<Integer> al = new ArrayList<Integer>();
-									al.add(arr[i]);
-									al.add(arr[i+1]);
-									al.add(arr[i+2]);
-									al.add(arr[i+3]);
-									al.add(arr[i+4]);
-									list.add(al);
-									System.out.println("here");
-									System.out.println(i);
-									/*
-									   char letc = let.charAt(0);
-									   int tempNum = letc;
-									int n = arr[i+4] - tempNum;
-									//System.out.println("N; " + n);
-									char f = ' ';	
-									int fi = f;
-									if(i == 0) {
-										if(arr[a-1] != arr[a+4+1]) {
-										word = "";
-												}
-										for(int t = a; t < a + 5; t++) {
-											if(arr[a-1] == arr[t]) {
-												word = "";
-											}
-											if(arr[a+4+1] == arr[t]) {
-												word = "";
-											}
-										}	
-									} else if((a + 5) == (arr.length - 1)) {
-										if(arr[i-1] != arr[i+4+1]) {
-											word = "";
-											}
-											for(int t = i; t < i + 5; t++) {
-												if(arr[i-1] == arr[t]) {
-													word = "";
-												}
-												if(arr[i+4+1] == arr[t]) {
-													word = "";
-												}
-
-											}
-									} else if(arr[i-1] != arr[i+4+1]) {
-										word = "";
-										for(int t = a; t < a + 5; t++) {
-											if(arr[a-1] == arr[t]) {
-												word = "";
-											}
-											if(arr[a+4+1] == arr[t]) {
-												word = "";
-											}
-										}
-									} else { 
-										word = "";
-										String aa = Integer.toString(arr[i]); 		
-										word+=aa;
-										word+=" ";
-										String b = Integer.toString(arr[i+1]); 
-										word+=b;
-										word+=" ";
-										String c = Integer.toString(arr[i+2]); 
-										word+=c;
-										word+=" ";
-										String d = Integer.toString(arr[i+3]); 
-										word+=d;
-										word+=" ";
-										String e = Integer.toString(arr[i+4]); 
-										word+=e;
-										word+=" ";
-										//System.out.println(word);
-										if((n + fi) == arr[i]) {
-											word = "";
-										} else if((n + fi)== arr[i+1]) {
-											word = "";
-										} else if((n + fi) == arr[i+2]) {
-											word = "";
-										} else if((n + fi) == arr[i+3]) {
-											word = "";
-										} else if((n + fi) == arr[i+4]) {
-											word = "";
-										} 
-										if(word != "") {
-											shifter = n;
-											//System.out.println("**" + word + "**" + n + "**" );
-											return shifter;
-										} 
-									}
-							*/	}
-							}
-						}
+			for(int a = i + len; a < (arr.length - len); a++) {
+				int counter = 0;
+				boolean matching = true;
+				while((counter < len) && (matching == true)) {
+					int tempA = i + counter;
+					int tempB = a + counter;
+					if(arr[tempA] != arr[tempB]) {
+						matching = false;
+					}
+					counter++;
+				}
+				if((matching == true)) {
+					ArrayList<Integer> al = new ArrayList<Integer>();
+					al.add(arr[i]);
+					al.add(arr[i+1]);
+					al.add(arr[i+2]);
+					al.add(arr[i+3]);
+					al.add(arr[i+4]);
+					list.add(al);
+					boolean foundWord = validDecrypt(al, i, a, arr, len, let);
+					if(foundWord == true) {
+						int offset = arr[i + len - 1] - letChar;
+						return offset;
 					}
 				}
-
 			}
-		}
-		for(int i = 0; i < list.size(); i++) {
-			ArrayList<Integer> temp = list.get(i);
-			for(int a = 0; a < temp.size(); a++) {
-				System.out.print(temp.get(a));
-			}
-				System.out.print('\n');
 		}
 
 		return shifter;
@@ -158,15 +73,87 @@ public class Main {
 
 		String line = s;
 		String message = line.substring(8, line.length());
-		//System.out.println(message);
 		String[] codesA = message.split(" ");
 		int[] codes = new int[codesA.length];
 		for(int i = 0; i < codes.length; i++) {
 			codes[i] = Integer.parseInt(codesA[i]);
-			//System.out.println(codes[i]);
 		}
 		s = "abc";
 		return codes;
+	}
+
+	private static boolean validDecrypt(ArrayList<Integer> al, int index, int indexTwo,  int[]
+	arr, int len, String let) {
+
+		boolean valid = true;
+
+		char letChar = let.charAt(0);
+		int pivotIndex = index;
+		int arrLen = arr.length;	
+		int offset = arr[index + len - 1] - letChar;
+
+		if(!isChar(arr[index + len - 1])) {
+			valid = false;
+			//System.out.println("offset means is space" + al);
+		}
+
+		boolean lastWord = false;
+		if(index == 0) {
+			pivotIndex = indexTwo;
+			if((indexTwo + len) == (arrLen - 1)) {
+				lastWord = true;
+			}
+		}
+		int endIndex = pivotIndex + len;
+
+		if((lastWord == false) && (valid == true)) {
+			int prevChar = arr[pivotIndex - 1];
+			int afterChar = arr[endIndex];
+			//System.out.println("prev char " + prevChar);
+			//System.out.println("last char " + afterChar);
+			if(prevChar != afterChar) {
+				valid = false;
+				//System.out.println("prev and after dont match");
+			} else{
+				if((prevChar - offset) != ' ') {
+					valid = false;
+					//System.out.println("first and after arent space");
+				} else {
+					for(int i = 0; i < len; i++) {
+						if((arr[pivotIndex + i] - offset) == ' ') {
+							valid = false;
+							//System.out.println("contaisn space");
+						}
+					}
+				}
+			}
+		} else {
+			return valid;
+		}
+		if(valid == true) {
+			//System.out.println("valid word");
+		} else {
+			//System.out.println("invalid word");
+		}
+		return valid;
+	}
+
+
+	public static boolean isChar(int n) {
+
+		if(n >= 72 && n <= 147) {
+			return true;
+		} else {
+			return false;
+		}
+		//return false;
+	}
+
+	public static void printCodes(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			 System.out.print(arr[i] + " ");
+		}
+		System.out.println();
 	}
 }
 
--- Java-Practice/LC-Subseq/Main.java
+++ Java-Practice/LC-Subseq/Main.java
@@ -33,16 +33,109 @@ public class Main {
 		if(s.charAt(0) == ';') {
 			return "";
 		}
-		if(s.charAt(s.length()-1) == ';') {
-			return "";
-		}
+		
 		String[] splitStrings = s.split(";");
 		String firstWord, secondWord;
+		String tfw, tsw;
+		tfw = "";
+		tsw = "";
 		int colonIndex = s.indexOf(';');
 		firstWord = splitStrings[0];
+		secondWord = "";
 		//firstWord = firstWord.toUpperCase();
-		secondWord = splitStrings[1];
+		int numOfSegs = splitStrings.length;
+		//System.out.println(numOfSegs);
+		String sequence = "";
+
+		for(int i = 0; i < numOfSegs - 1; i++) {
+			tfw="";
+			tsw="";
+			for(int a = 0; a <= i; a++) {
+				tfw+= splitStrings[a];
+				if(a < i) {
+					tfw+=";";
+				}
+			}
+
+			for(int b = i + 1; b < numOfSegs; b++) {
+				tsw+=splitStrings[b];
+				if(b < numOfSegs - 1) {
+					tsw+=";";
+				}
+				if(b == numOfSegs - 1) {
+					if(s.charAt(s.length()-1) == ';') {
+						//return "";
+						tsw+=";";
+					}				
+				}
+
+			}
+
+
+			//System.out.println("first word " + tfw);
+			//System.out.println("second word " + tsw);
+			String currentSeq = getMaxString(tfw, tsw);
+
+			int currentLen = currentSeq.length();
+			int maxLen = sequence.length();
+			if(currentLen > maxLen) {
+				sequence = currentSeq;
+			}
+
+		}
+/*
+		for(int i = 1; i < splitStrings.length; i++) {
+			String tempArrayString = splitStrings[i];
+			//System.out.println(tempArrayString); 
+			secondWord+=tempArrayString;
+			if(i < splitStrings.length - 1) {
+				secondWord+=";";
+			}
+			//System.out.println(secondWord);
+		} */
+		//secondWord = splitStrings[1];
 		//secondWord = secondWord.toUpperCase();
+				/*
+		int sequenceLength = -1;
+		int firstWordLength = firstWord.length();
+		int secondWordLength = secondWord.length();
+		int firstCounter = 0;
+		int secondCounter = 0;
+		for(int i = 0; i < firstWordLength;i++) {
+			String tempString = "";	
+			firstCounter = i;
+			secondCounter = 0;
+			int tempCounterSpot = 0;
+			while((firstCounter < firstWordLength) && (secondCounter < secondWordLength)) {
+				if(firstWord.charAt(firstCounter) == secondWord.charAt(secondCounter)) {
+
+					tempString+=firstWord.charAt(firstCounter);
+					firstCounter++;
+					secondCounter++;
+					tempCounterSpot = secondCounter;
+				} else {
+					secondCounter++;	
+				}
+				if(tempString.length() > sequenceLength) {
+					sequence = tempString;
+					sequenceLength = tempString.length();
+				} 
+				if(secondCounter > (secondWordLength - 1)) {
+					firstCounter++;
+					secondCounter = tempCounterSpot;
+				}
+
+			}
+		}
+		 */
+
+		return sequence;
+	}
+
+	public static String getMaxString(String fw, String sw) {
+
+		String firstWord = fw;
+		String secondWord = sw;
 		String sequence = "";
 		int sequenceLength = -1;
 		int firstWordLength = firstWord.length();
